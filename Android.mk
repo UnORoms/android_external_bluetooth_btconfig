ff6c9be40f6b1b49b29e7045eb40e4bb1ff3a013
Liang Ying <liangy@codeaurora.org>
2014-03-27 13:41:51 -0700

    Add support of btconfig for ROME over UART
    OEM needs a tool that communicate with SoC via sending/receiving
    HCI commands/events. In BlueZ, we have hcitool. After migrating
    to Bluedroid, we use 'btconfig' as a replacement of 'hcitool' in
    BlueZ. However, currently btconfig can only support AR3002 and
    WCN solution. OEM need the btconfig working on ROME over UART.
    
    CRs-fixed: 629317
    
    Change-Id: Id2b4d6dddcdf912b92148b9ae961a4e640ff6be3

--- Android.mk
+++ Android.mk
@@ -3,15 +3,20 @@ ifeq ($(BOARD_HAVE_BLUETOOTH),true)
 LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
+BDROID_DIR:= external/bluetooth/bluedroid
+
+LOCAL_C_INCLUDES := $(BDROID_DIR)/hci/include
+
 LOCAL_SRC_FILES:= \
-	btconfig.c
+              btconfig.c
 
 LOCAL_MODULE_PATH := $(TARGET_OUT_OPTIONAL_EXECUTABLES)
 LOCAL_MODULE_TAGS := debug
 LOCAL_MODULE:=btconfig
 
 LOCAL_SHARED_LIBRARIES += libcutils   \
-                          libutils
+                          libutils    \
+                          libdl
 
 include $(BUILD_EXECUTABLE)
 endif
